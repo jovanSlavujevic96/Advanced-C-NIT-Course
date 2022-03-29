06d2103339c83a0d80f2b25277ca9302f0268a58
Jovan Slavujevic <jotzilla96@gmail.com>
2022-03-29 07:29:46 +0000

    M1 - Exercise 0

---
+++ lab00/task1/hello.c
@@ -0,0 +1,22 @@
+/**
+ * @author Jovan Slavujevic
+ *
+ * @brief Exercise 0 - Setting up the development environment
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    // iterator variable
+    int i;
+
+    // printing loop
+    for (i = 0; i < 3; ++i)
+    {
+        printf("Hello world!\n");
+    }
+
+    // end
+    return 0;
+}
