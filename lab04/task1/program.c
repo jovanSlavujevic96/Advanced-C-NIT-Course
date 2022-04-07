47f6d4d08b9114c31bcae14b164b758480cbd18f
Jovan Slavujevic <jotzilla96@gmail.com>
2022-04-07 07:19:02 +0000

    M4 Exercises Review

--- lab04/task1/program.c
+++ lab04/task1/program.c
@@ -1,3 +1,34 @@
+/**
+ * @title Taks 1
+ *
+ * @brief This is continuation on the program in program.c file from the previous exercises.This time the program is changed in the following ways. Instead of two arrays – one for values,
+ * and one for initial indexes – now there is only one array of structures with separate fields for value and index. Additionally, now a user can select the threshold, as well as algorithm
+ * which will be used for sorting: bubble sort or quick sort.
+ *
+ * Here are the tasks:
+ * 1) From within function quick_sort, for which the empty body is given, call standard library function qsort. Here is the description of qsort function, copied directly from C99 standard.
+ * qsort Synopsis
+ *
+ * #include <stdlib.h>
+ * void qsort(void* base, size_t nmemb, size_t size, int (*compar)(const void*, const void*));
+ *
+ * Description
+ * The qsort function sorts an array of nmemb objects, the initial element of which is pointed to by base. The size of each object is specified by size.
+ *
+ * The contents of the array are sorted into ascending order according to a comparison function pointed to by compar, which is called with two arguments that point to the objects being compared.
+ * The function shall return an integer less than, equal to, or greater than zero if the first argument is considered to be respectively less than, equal to, or greater than the second.
+ *
+ * If two elements compare as equal, their order in the resulting sorted array is unspecified.
+ *
+ * The qsort function returns no value.
+ *
+ * 2) Remove switch statements from code, and implement their functionality (which is selection of the threshold and sorting function) in a different way (Hint: look-up/jump tables).
+ *
+ * 3) Enter the following 10 element array: 41, 55, 27, 33, 33, 33, 27, 79, 15, 50. Select the first threshold (with the index 1). Compare results with both sorting algorithms. Analyze what you observe.
+ *
+ * @author Jovan Slavujevic
+ */
+
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -29,7 +60,7 @@ static void bubble_sort(val_ind_struct_t* str_array, int_least16_t n)
 			if (str_array[i].value < str_array[j].value)
 			{
 				val_ind_struct_t tmp;
-				
+
 				tmp = str_array[i];
 				str_array[i] = str_array[j];
 				str_array[j] = tmp;
@@ -39,32 +70,38 @@ static void bubble_sort(val_ind_struct_t* str_array, int_least16_t n)
 }
 
 
+static int comparator(const void* l, const void* r)
+{
+	int ret = 0;
+	const val_ind_struct_t* left  = (val_ind_struct_t*)l;
+	const val_ind_struct_t* right = (val_ind_struct_t*)r;
+
+	if (left->value < right->value)
+	{
+		ret = 1;
+	}
+	else if (left->value < right->value)
+	{
+		ret = -1;
+	}
+	return ret;
+}
+
 static void quick_sort(val_ind_struct_t* str_array, int_least16_t n)
 {
 	// sort using qsort standard function
+	qsort(struct_array, n, sizeof(val_ind_struct_t), comparator);
 }
 
 
 static void print(const val_ind_struct_t* str_array, int_least16_t n, int_fast8_t tr_index)
 {
 	int_least16_t i = 0;
-	int_least32_t treshold;
 	printf("Up to the %"PRIdFAST8". treshold: ", tr_index);
 
-	switch (tr_index)
-	{
-	case 1:
-		treshold = treshold1;
-		break;
-	case 2:
-		treshold = treshold2;
-		break;
-	case 3:
-		treshold = treshold3;
-		break;
-	}
+	const int_least32_t tresholds[] = {treshold1, treshold2, treshold3}; // look-up table
 
-	while (str_array[i].value > treshold && i < n)
+	while (str_array[i].value > tresholds[tr_index-1] && i < n)
 	{
 		printf("%"PRIdLEAST32"(%"PRIdLEAST16") ", str_array[i].value, str_array[i].index);
 		i++;
@@ -79,9 +116,13 @@ void main()
 	int_least16_t n;
 	int_fast8_t treshold_index;
 	int_fast8_t sort_index;
+
+	void (*sort_func_table[])(val_ind_struct_t*, int_least16_t) = {bubble_sort, quick_sort}; // jump table
+
 	printf("Number of elements: ");
 	scanf("%"SCNdLEAST16, &n);
 	printf("\n");
+
 	for (i = 0; i < n; i++)
 	{
 		printf("%"PRIdLEAST16". element: ", i);
@@ -89,22 +130,16 @@ void main()
 		struct_array[i].index = i;
 		printf("\n");
 	}
+
 	printf("Trashold you want to use (1, 2, or 3): ");
 	scanf("%"SCNdFAST8, &treshold_index);
 	printf("\n");
+
 	printf("Sorting algorithm you want to use (1- bubble, 2- quick): ");
 	scanf("%"SCNdFAST8, &sort_index);
 	printf("\n");
 
-	switch (sort_index)
-	{
-	case 1:
-		bubble_sort(struct_array, n);
-		break;
-	case 2:
-		quick_sort(struct_array, n);
-		break;
-	}
+	sort_func_table[sort_index-1](struct_array, n);
 
 	print(struct_array, n, treshold_index);
 }
--- lab04/task2/e_bitmap.c
+++ lab04/task2/e_bitmap.c
@@ -1,16 +1,77 @@
+/**
+ * @author Jovan Slavujevic
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <inttypes.h>
 #include "bmp.h"
 #include "imageProcessing.h"
 
 static const int ARG_NUM = 2;
+
+static void calculateAverageRGB(const uint_least8_t* bitmapData, int_least32_t width, int_least32_t height,
+	uint_least8_t* const red_out, uint_least8_t* const green_out, uint_least8_t* const blue_out)
+{
+	int_least32_t i = 0;
+	uint_least32_t average_red = 0;
+	uint_least32_t average_green = 0;
+	uint_least32_t average_blue = 0;
+
+	for (i = 0; i < width * height * 3; i += 3)
+	{
+		average_red   += bitmapData[i];
+		average_green += bitmapData[i+1];
+		average_blue  += bitmapData[i+2];
+	}
+
+	average_red   /= (width * height);
+	average_green /= (width * height);
+	average_blue  /= (width * height);
+
+	*red_out = average_red;
+	*green_out = average_green;
+	*blue_out = average_blue;
+}
+
+static void mean_filter(const uint_least8_t* bitmapData, uint_least8_t* outData, int_least32_t width, int_least32_t height)
+{
+	int_least32_t i,j,k,l,m;
+	uint_leaset16_t sum = 0; // max values 255 * 9 > 2^8 && < 2^16
+
+	for (i=1; i<iHeight-1; i++)
+	{
+		for (j=1; j<iWidth-1; j++)
+		{
+			for (k=0; k<3; k++)
+			{
+				for (l=-1; l <= 1; l++)
+				{
+					for (m=-1; m <= 1; m++)
+					{
+						sum += iData[(i+l) * iWidth * 3 + (j+m) * 3 + k];
+					}
+				}
+				iData[i*iWidth*3 + j*3 + k] = sum/9;
+				sum = 0;
+			}
+		}
+	}
+}
+
 
 int main(int argc, char* argv[])
 {
 	int_fast32_t err = 0;
 	BITMAPFILEHEADER bitmapFileHeader;
 	BITMAPINFOHEADER bitmapInfoHeader;
-	uint_least8_t* bitmapData;
+	uint_least8_t* bitmapData = NULL;
+	uint_least8_t* meanData = NULL;
+
+	uint_least8_t red_average = 0;
+	uint_least8_t green_average = 0;
+	uint_least8_t blue_average = 0;
+
 
 	// Check the input arguments
 	if ( argc != ARG_NUM )
@@ -18,11 +79,11 @@ int main(int argc, char* argv[])
 		printf("Enter input file path \n");
 		return EXIT_FAILURE;
 	}
-	
+
 	/////////////////////////////////////////////////////////////////////////////////////////////////////
 	// Example #1 - Apply brightness effect
 	printf("Applying brightness\n");
-	
+
 	// Load image
 	bitmapData = LoadBitmapFile(argv[1],&bitmapFileHeader, &bitmapInfoHeader);
 	if(!bitmapData)
@@ -33,21 +94,21 @@ int main(int argc, char* argv[])
 
 	// Invoke processing function
 	adjustImageBrightness(bitmapData, bitmapInfoHeader.biWidth, bitmapInfoHeader.biHeight, false, 70);
-	
+
 	// Save the result
 	err = SaveBitmapFile("out_brightness.bmp", &bitmapFileHeader, &bitmapInfoHeader, bitmapData);
-	if (err) 
+	if (err)
 	{
 		printf("Error trying to save bitmap\n");
 	}
-	
+
 	// Free image data
 	free(bitmapData);
 
 	/////////////////////////////////////////////////////////////////////////////////////////////////////
 	// Example #2 - Apply black and white effect
 	printf("Applying BW image\n");
-	
+
 	// Load image
 	bitmapData = LoadBitmapFile(argv[1], &bitmapFileHeader, &bitmapInfoHeader);
 	if(!bitmapData)
@@ -55,24 +116,24 @@ int main(int argc, char* argv[])
 		printf("Error! Can't open input bitmap file: %s\n", argv[1]);
 		return EXIT_FAILURE;
 	}
-	
+
 	// Invoke processing function
 	bwImage(bitmapData, bitmapInfoHeader.biWidth, bitmapInfoHeader.biHeight);
-	
+
 	// Save the result
 	err = SaveBitmapFile("out_bw.bmp", &bitmapFileHeader, &bitmapInfoHeader, bitmapData);
 	if (err)
 	{
 		printf("Error trying to save bitmap\n");
 	}
-	
+
 	// Free image data
 	free(bitmapData);
-	
+
 	/////////////////////////////////////////////////////////////////////////////////////////////////////
 	// Example #3 - Apply color effect
 	printf("Applying effect\n");
-	
+
 	// Load image
 	bitmapData = LoadBitmapFile(argv[1], &bitmapFileHeader, &bitmapInfoHeader);
 	if(!bitmapData)
@@ -80,21 +141,40 @@ int main(int argc, char* argv[])
 		printf("Error! Can't open input bitmap file: %s\n", argv[1]);
 		return EXIT_FAILURE;
 	}
-	
+
 	// Invoke processing function
 	effect(bitmapData, bitmapInfoHeader.biWidth, bitmapInfoHeader.biHeight);
-	
+
 	// Save the result
 	err = SaveBitmapFile("out_effect.bmp", &bitmapFileHeader, &bitmapInfoHeader, bitmapData);
-	if (err) 
+	if (err)
 	{
 		printf("Error trying to save bitmap\n");
 	}
-	
+
 	// Free image data
 	free(bitmapData);
 
 	/////////////////////////////////////////////////////////////////////////////////////////////////////
+	/// TASK 1
+	bitmapData = LoadBitmapFile(argv[1], &bitmapFileHeader, &bitmapInfoHeader);
+	if(!bitmapData)
+	{
+		printf("Error! Can't open input bitmap file: %s\n", argv[1]);
+		return EXIT_FAILURE;
+	}
+
+	calculateAverageRGB(bitmapData, bitmapInfoHeader.biWidth, bitmapInfoHeader.biHeight, &red_average, &green_average, &blue_average);
+
+	printf("RED AVERAGE   = %" SCNuLEAST8 "\n", red_average);
+	printf("GREEN AVERAGE = %" SCNuLEAST8 "\n", green_average);
+	printf("BLUE AVERAGE  = %" SCNuLEAST8 "\n", blue_average);
 	
+	/////////////////////////////////////////////////////////////////////////////////////////////////////
+	/// TASK 1
+
+	// Free image data
+	free(bitmapData);
+
 	return EXIT_SUCCESS;
 }
